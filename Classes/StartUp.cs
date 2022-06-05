b298caa8b7ba00f6fd9986aa6f1102327847b2fa
Ana Maria <anamaria27988@gmail.com>
2022-06-05 23:29:20 +0300

    Add project files.

---
+++ Classes/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] splitted = Console.ReadLine().Split();
+                string name = splitted[0];
+                int age = int.Parse(splitted[1]);
+
+                Person person = new Person(name, age);
+                people.Add(person);
+            }
+
+            List<Person> modified = people.Where(p => p.Age > 30).OrderBy(n => n.Name).ToList();
+            foreach (var item in modified)
+            {
+
+
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+
+        }
+    }
+}
---
+++ Oldest FamilyMember/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int age = int.Parse(input[1]);
+                string name = input[0];
+                Person person = new Person(name, age);
+                family.AddMember(person);
+
+            }
+
+            Person oldest = family.GetOldestMember();
+            Console.WriteLine(oldest.Name + " " + oldest.Age);
+            //var person = new Person()
+            //{
+            //    Name = "Peter",
+            //Age = 20,
+            //};
+
+            //Person anotherPerson = new Person();
+
+            //Person thirdPerson = new Person(2);
+            //Person Sam = new Person("Sam", 3);
+        }
+    }
+}
